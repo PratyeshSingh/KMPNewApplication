23c4dbec37ec33c3f96dabc4c47f8a73f392c629
pratyesh <pratyesh3@gmail.com>
2026-07-17 10:03:34 +0530

    loading optimise

--- features/product-details/src/commonMain/kotlin/com/product/details/ProductDetailsScreen.kt
+++ features/product-details/src/commonMain/kotlin/com/product/details/ProductDetailsScreen.kt
@@ -2,19 +2,24 @@ package com.product.details
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -42,7 +47,7 @@ fun ProductDetailsScreen(
     }
     val product = viewModel.state.collectAsStateWithLifecycle().value
 
-    AnimatedContent(product != null) {
+    AnimatedContent(true) {
         Scaffold(
             topBar = topBar,
             containerColor = MaterialTheme.colorScheme.background
@@ -53,6 +58,18 @@ fun ProductDetailsScreen(
                     onClick = onClick,
                     item = product
                 )
+            }?:run {
+                Box(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(50.dp),
+                        color = Color.Red
+                    )
+                }
             }
         }
     }
